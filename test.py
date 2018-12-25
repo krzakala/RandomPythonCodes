ac9ad5a62130c134bc782e100f9292fa69104554
Florent Krzakala <florent.krzakala@gmail.com>
2018-12-25 17:31:09 +0100

    First commit

---
+++ test.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.subplot(1,2,1)
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+
+
+plt.subplot(1,2,2)
+plt.plot([1,2,3,4])
+
+
+plt.show()
+
