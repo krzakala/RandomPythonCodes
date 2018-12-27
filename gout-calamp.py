f85c4720840955f18847754ae66fc83335a80633
Florent Krzakala <florent.krzakala@gmail.com>
2018-12-27 20:20:11 +0100

    Corrected gout
    Now it works

--- gout-calamp.py
+++ gout-calamp.py
@@ -14,34 +14,33 @@ def integral_p(N, mu, sigma, a, b):
     result = 0
     for i in range(N + 1):
         gam = gamma(i / 2. + 0.5)
-        gam_ib = gammainc((b - mu)**2 / (2 * sigma), i / 2. + 0.5)
-        gam_ia = gammainc((a - mu)**2 / (2 * sigma), i / 2. + 0.5)
+        gam_ib = gammainc(i / 2. + 0.5, (b - mu)**2 / (2 * sigma))
+        gam_ia = gammainc(i / 2. + 0.5, (a - mu)**2 / (2 * sigma))
         if (i % 2 == 0):
             incr = 0.5 * gam * (sign(b - mu) * gam_ib - sign(a - mu) * gam_ia)
         else:
-            incr = 0.5 * gam * (gam_ib - gam_ia)
 
+            incr = 0.5 * gam * (gam_ib - gam_ia)
         result = result + \
             comb(N, i) * mu**(N - i) * (sqrt(2 * sigma))**(i + 1) * incr
-
     return result
 
 
-P = 5
-wd = 1
-MD = 1
-VD = 0.1
-eps = 1E-8
+def posterior_m_and_v_on_d(P, wd, MD, VD):
+    eps = 1E-8
+    binf = (2 - wd) / 2.
+    bsup = (2 + wd) / 2.
 
-binf = (2 - wd) / 2.
-bsup = (2 + wd) / 2.
+    int0 = integral_p(P, MD, VD, binf, bsup)
+    int1 = integral_p(P + 1, MD, VD, binf, bsup)
+    int2 = integral_p(P + 2, MD, VD, binf, bsup)
 
-int0 = integral_p(P, MD, VD, binf, bsup)
-int1 = integral_p(P + 1, MD, VD, binf, bsup)
-int2 = integral_p(P + 2, MD, VD, binf, bsup)
+    md = int1 / max(int0, eps)
+    vd = max(int2 / max(int0, eps) - md**2, eps)
 
-md = int1 / max(int0, eps)
-vd = int2 / max(int0, eps) - md**2
+    print(int1, int2, int0)
+    return md, vd
 
-print("Test ok :-)")
-print(md, vd)
+
+(m, v) = posterior_m_and_v_on_d(2, 1, 1.5100000, 1)
+print(m, v)
--- test.py
+++ test.py
@@ -1,16 +1,2 @@
-from scipy.misc import comb
-
-
-def nchoosek(n, k):
-    if k == 0:
-        r = 1
-    else:
-        r = n / k * nchoosek(n - 1, k - 1)
-    return round(r)
-
-
-print(nchoosek(3, 2))
-print(comb(3, 2))
-
-
-
+import scipy
+print(scipy.__version__)
